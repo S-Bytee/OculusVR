da3558b1a4d85476091bddc128988d4951ea3285
Outail619 <47230435+Outail619@users.noreply.github.com>
2020-04-24 21:56:17 +0100

    Main menu / Authentification

---
+++ Assets/User/Scripts/Login.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB;
+
+public class Login : MonoBehaviour
+{
+
+    public GameObject input_email;
+    public GameObject input_password;
+    private string email;
+    private string password;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         email = input_email.GetComponent<InputField>().text;
+         password = input_password.GetComponent<InputField>().text;
+         if(Input.GetKeyDown(KeyCode.Tab)){changeInput();}
+         if(Input.GetKeyDown(KeyCode.Return)){singin();}
+        
+    }
+
+    void singin(){
+
+    }
+
+    public void changeInput()
+    {
+            if(input_email.GetComponent<InputField>().isFocused){
+                input_password.GetComponent<InputField>().Select();
+            }
+    }
+}
