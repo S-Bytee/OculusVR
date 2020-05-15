2ca2245ec8c1afcfc7e366676a1a091a164f0a34
akthemarfaoui <47156414+akthemarfaoui@users.noreply.github.com>
2020-05-16 00:36:52 +0100

    LoadProject2D_InProgress

---
+++ Assets/Dessin2D/Scripts/LoadProject2D.cs
@@ -0,0 +1,53 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class LoadProject2D : MonoBehaviour
+{
+    public GameObject project;
+    GameObject currProject;
+    List<string> collectionNames = new List<string>();
+    string username;
+
+
+    void Start()
+    {
+        username = PlayerPrefs.GetString("username");
+        instanciateProject();
+
+    }
+    public void instanciateProject()
+    {
+        getAllProjects();
+
+        for (int i = 0; i < collectionNames.Count; i++)
+        {
+            string collectionName = collectionNames.ElementAt(i).ToString().Split('_')[0];
+            if (collectionNames.ElementAt(i).ToString().Split('_')[1].ToString()=="2D")
+            {
+                Debug.Log(collectionName);
+
+            }
+
+            //currProject = Instantiate(project, Vector3.zero, Quaternion.identity);
+           // currProject.GetComponent<Button>().name = collectionName;
+           // currProject.transform.GetChild(0).GetComponent<Text>().text = collectionName;
+           // currProject.transform.SetParent(transform.GetChild(0).GetChild(0).GetComponent<VerticalLayoutGroup>().transform, false);
+
+        }
+
+
+    }
+    public void getAllProjects()
+    {
+        foreach (var item in Mongo.getConnection().GetDatabase(username).ListCollectionsAsync().Result.ToListAsync<BsonDocument>().Result)
+        {
+            if (!item["name"].ToString().Equals("Default"))
+                collectionNames.Add(item["name"].ToString());
+        }
+
+    }
+}
