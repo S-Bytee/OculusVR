344fb340ed3446db712e6a485099948c22780c50
Outail619 <47230435+Outail619@users.noreply.github.com>
2020-06-13 01:42:12 +0100

    Update all scenes with new player prefab

--- Assets/Script/RoomScript.cs
+++ Assets/Script/RoomScript.cs
@@ -8,17 +8,31 @@ public class RoomScript : MonoBehaviour
     public GameObject users_book;
     public GameObject friend_phone;
     public GameObject hoverSound;
+    PhysicsPointer laserPointer;
     // Start is called before the first frame update
     void Start()
     {
+        laserPointer = PhysicsPointer.Instance;
         GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("MusicVolume");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown (0)) {    
-             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (laserPointer.hit.collider && Input.GetMouseButtonDown(0)) {
+
+            if(laserPointer.hit.collider.name == "book4")
+            {
+                PlayerPrefs.SetString("scene", "book_scene");
+                SceneManager.LoadScene("loading_screen");
+            }else if (laserPointer.hit.collider.name == "Dimkra_Dveri_Klasik_Anya3")
+            {
+                PlayerPrefs.SetString("scene", "Menu_Scene");
+                SceneManager.LoadScene("loading_screen");
+            }
+
+
+             /*var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
              RaycastHit hit;
             print("no");
              if (Physics.Raycast(ray, out hit)) {
@@ -32,7 +46,7 @@ public class RoomScript : MonoBehaviour
                      SceneManager.LoadScene("loading_screen");
                  }
                  
-             }    
+             }   */ 
          }
          
     }
